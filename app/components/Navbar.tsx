857d52ec0f97c2b179c820b9ab07b7d2dee8a320
httpArbaz <128707115+httpArbaz@users.noreply.github.com>
2023-08-06 16:17:22 +0500

    build fixes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,43 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='hidden lg:block'>
-        <div className="container">
-            <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
-                <Link className="navbar__link relative" href={"#"}>HOME</Link>
-                <Link className="navbar__link relative" href={"#"}>CATEGORIES</Link>
-                <Link className="navbar__link relative" href={"#"}>MEN'S</Link>
-                <Link className="navbar__link relative" href={"#"}>WOMEN'S</Link>
-                <Link className="navbar__link relative" href={"#"}>JEWELERY</Link>
-                <Link className="navbar__link relative" href={"#"}>PERFUME</Link>
-                <Link className="navbar__link relative" href={"#"}>BLOG</Link>
-                <Link className="navbar__link relative" href={"#"}>HOT OFFER</Link>
-            </div>
+    <div className="hidden lg:block">
+      <div className="container">
+        <div className="flex w-fit gap-10 mx-auto font-medium py-4 text-blackish">
+          <Link className="navbar__link relative" href={"#"}>
+            HOME
+          </Link>
+          <Link className="navbar__link relative" href={"#"}>
+            CATEGORIES
+          </Link>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <Link className="navbar__link relative" href="#">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            MEN'S
+          </Link>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <Link className="navbar__link relative" href="#">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            WOMEN'S
+          </Link>
+          <Link className="navbar__link relative" href={"&apos;"}>
+            JEWELERY
+          </Link>
+          <Link className="navbar__link relative" href={"&apos;"}>
+            PERFUME
+          </Link>
+          <Link className="navbar__link relative" href={"&apos;"}>
+            BLOG
+          </Link>
+          <Link className="navbar__link relative" href={"&apos;"}>
+            HOT OFFER
+          </Link>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
